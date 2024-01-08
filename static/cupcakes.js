0d3b8885f028925b57d5aedad267a1d2aba5db6a
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-07 18:05:18 -0800

    cupcake submission now displays properly

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,43 +1,57 @@
 const cupcakesList = document.querySelector("#cupcakes-list");
-const cupcakesForm = document.querySelector("#cupcakes-form")
+const cupcakesForm = document.querySelector("#cupcakes-form");
+
+// form fields
+const flavorField = cupcakesForm.querySelector("#flavor-field")
+const sizeField = cupcakesForm.querySelector("#size-field")
+const ratingField = cupcakesForm.querySelector("#rating-field")
+const imageField = cupcakesForm.querySelector("#image-field")
+
+function displayCupcake(cupcake) {
+    const entry = document.createElement('li')
+        
+    // add the image
+    const img = document.createElement('img')
+    img.src = cupcake.image
+    img.height = 150
+    entry.appendChild(img)
+    
+    // add the rest of the info
+    const textSection = document.createElement('a') // using an anchor tag so we can link to an indvidual page later
+    textSection.innerText = `Flavor: ${cupcake.flavor}, Size: ${cupcake.size}, Rating: ${cupcake.rating}`
+    entry.appendChild(textSection)
+    
+    cupcakesList.appendChild(entry)
+}
 
 async function get_cupcakes() {
     cupcakes = await axios.get("/api/cupcakes")
     for(let c of cupcakes.data.cupcakes) {
-        const entry = document.createElement('li')
-        
-        // add the image
-        const img = document.createElement('img')
-        img.src = c.image
-        img.height = 150
-        entry.appendChild(img)
-        
-        // add the rest of the info
-        const textSection = document.createElement('a') // using an anchor tag so we can link to an indvidual page later
-        textSection.innerText = `Flavor: ${c.flavor}, Size: ${c.size}, Rating: ${c.rating}`
-        entry.appendChild(textSection)
-        
-        cupcakesList.appendChild(entry)
+        displayCupcake(c)
     }
 }
 
 async function submit_cupcake(evt) {
     evt.preventDefault()
     
-    // form fields
-    const flavorField = cupcakesForm.querySelector("#flavor-field")
-    const sizeField = cupcakesForm.querySelector("#size-field")
-    const ratingField = cupcakesForm.querySelector("#rating-field")
-    const imageField = cupcakesForm.querySelector("#image-field")
-    
     const formData = {
         flavor: flavorField.value,
         size: sizeField.value,
         rating: ratingField.value,
-        image: imageField.value,
+    }
+    if (imageField.value) {
+        formData.image = imageField.value
     }
     
-    newCupcake = await axios.post("/api/cupcakes", formData)
+    console.log(formData.image)
+    
+    try {
+        newCupcake = await axios.post("/api/cupcakes", formData)
+        displayCupcake(newCupcake.data.cupcake)
+    }
+    catch {
+        alert("Cupcake post failed")
+    }
 }
 
 get_cupcakes();

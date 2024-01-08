f7058a0d29ec899c59de64f5c31dde9ad31b81c1
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-01-07 16:37:32 -0800

    added post route

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 """Flask app for Cupcakes"""
 
 # Flask imports
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 # Local imports
-from models import connect_db, Cupcake
+from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
@@ -32,3 +32,30 @@ def get_all_cupcakes():
     serialized = [c.to_dict() for c in cupcakes]
     
     return jsonify(cupcakes=serialized)
+
+@app.route("/api/cupcakes/<cupcake_id>")
+def get_cupcake(cupcake_id):
+    '''returns JSON of a single cupcake'''
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.to_dict()
+    
+    return jsonify(cupcake=serialized)
+
+@app.route("/api/cupcakes", methods=["POST"])
+def add_cupcake():
+    '''adds a cupcake to the database. returns the JSON of that cupcake'''
+    
+    # Pull the parameters from the JSON
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json.get("image", None)
+    
+    # Make the cupcake and add to the DB
+    new_cupcake = Cupcake(flavor=flavor, size=size, rating=rating, image=image)
+    db.session.add(new_cupcake)
+    db.session.commit()
+    
+    serialized = new_cupcake.to_dict
+    # Return with status code 201
+    return ( jsonify(cupcake = serialized), 201 )
